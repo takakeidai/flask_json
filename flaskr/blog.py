a1eed8a012c92e159663f06f24689d56d7fa0d1a
takahiro-s <pekeidai.ts@gmail.com>
2021-12-02 23:49:11 +0900

    JSON形式のFlask
    第一弾

---
+++ flaskr/blog.py
@@ -0,0 +1,128 @@
+
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
+)
+from werkzeug.exceptions import abort
+
+from werkzeug.security import check_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+# @bp.route('/')
+# def index():
+#     db = get_db()
+#     # posts = db.execute(
+#     #     'SELECT p.id, title, body, created, author_id, username'
+#     #     ' FROM post p JOIN user u ON p.author_id = u.id'
+#     #     ' ORDER BY created DESC'
+#     # ).fetchall()
+#     posts = db.execute(
+#         'SELECT id, title, body, created'
+#         ' FROM post'
+#         ' ORDER BY created DESC'
+#     ).fetchall()
+    
+#     posts_list_without_index = []
+#     for post in posts:
+#         posts_list_without_index.append(
+#             dict(
+#                 id = post['id'],
+#                 title = post["title"],
+#                 body = post["body"],
+#                 created_at = post["created"]
+#                 )
+#             )
+#     return jsonify(posts_list_without_index)
+
+
+@bp.route('/blog', methods = ('GET','POST'))
+def index_and_create():
+    if request.method == 'POST':
+        res = request.get_json()
+        title = res['title']
+        body = res['body']
+        error = None
+        
+        if not title:
+            error = 'Title is required.'
+            
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body)'
+                ' VALUES (?, ?)',
+                (title, body)
+            )
+            db.commit()
+            message = {
+                "message" : "new post is successfully created."
+            }
+            return jsonify(message)
+        
+    db = get_db()
+    posts = db.execute(
+        'SELECT id, title, body, created'
+        ' FROM post'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    
+    posts_list_without_index = []
+    for post in posts:
+        posts_list_without_index.append(
+            dict(
+                id = post['id'],
+                title = post["title"],
+                body = post["body"],
+                created_at = post["created"]
+                )
+            )
+    return jsonify(posts_list_without_index)
+            
+    # return redirect(url_for('blog.index'))
+        
+
+
+
+@bp.route('/blog/<int:id>', methods = ('GET', 'PUT', 'DELETE'))
+def update_and_delete(id):
+    
+    if request.method == 'PUT':
+        res = request.get_json()
+        title = res['title']
+        body = res['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            message = {
+                "message" : "your post is updated."
+            }
+            return jsonify(message)
+    
+    # DELETEの時、content-Typeに、「charset=utf-8」と設定しなければいけない。
+    if request.method == 'DELETE':
+        db = get_db()
+        db.execute('DELETE FROM post WHERE id = ?', (id,))
+        db.commit()
+        message = {
+            "message" : "your post is deleted."
+        }
+        return jsonify(message)
+    
+    return redirect(url_for('blog.index'))
